4811986c5e1c4b371fb9e44d5f171eacce9bc01b
Kiranjeet Kaur <741277@student.tor.loc>
2019-11-22 15:54:42 -0500

    first commit

--- add.js
+++ add.js
@@ -1,14 +1,15 @@
 //program to add numbers from 100 to 1 using for loop
 
 var i=0;sum=0;
-for(i=1;i<=100;i++){
+for(i=100;i>=1;i--){
     sum=sum+i;
 }
 console.log("Sum of numbers from 100 to 1 is :"+sum)
 
-//program to add numbers from 100 to 1 using for loop
+//program to add numbers from 500to 114 using for loop
+
 var j=0;add=0;
-for(j=114;j<=500;j++){
+for(j=500;j>=114;j--){
     add=add+j;
 }
 console.log("Sum of numbers from 500 to 114 is :"+add)
